3e98091b70d37cefac8d49b44caf28339942daa5
LumreLars <gustavfl13@gmail.com>
2025-09-03 08:12:42 +0200

    Opgave 1

--- src/opgave01/CalculateVat.java
+++ src/opgave01/CalculateVat.java
@@ -3,7 +3,21 @@ package opgave01;
 import java.util.Scanner;
 
 public class CalculateVat {
+
+    public static double momsBeregner(double beløb){
+        return beløb * 0.25;
+    }
+
     public static void main(String[] args) {
-        //Beskriv din algoritme
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Skriv dit beløb før moms: ");
+        double beløb = scanner.nextDouble();
+
+        double momsBeløb = momsBeregner(beløb);
+        double total = momsBeløb + beløb;
+
+        System.out.println("Her er moms beløbet: " + momsBeløb + " Kr.");
+        System.out.println("Her er beløbet med moms inkluderet: " + total + " Kr.");
+        scanner.close();
     }
 }
