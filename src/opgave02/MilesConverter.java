5726b01fca52125477b555542cbbdcb15bd3c54c
LumreLars <gustavfl13@gmail.com>
2025-09-03 08:32:44 +0200

    Opgave 2

--- src/opgave02/MilesConverter.java
+++ src/opgave02/MilesConverter.java
@@ -4,6 +4,20 @@ import java.util.Formatter;
 import java.util.Scanner;
 
 public class MilesConverter {
-    public static void main(String[] args) {
+
+static double convert(double miles){
+
+        return miles * 1.6;
     }
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Skriv et antal mil, for at convertere til km: ");
+        double x = scanner.nextInt();
+        scanner.close();
+
+        double y = convert(x);
+
+
+        System.out.println("Antal km: " + y);
+    }
+}   
