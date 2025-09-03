1ab432fc870e787b8af666257af80d94daccb405
LumreLars <gustavfl13@gmail.com>
2025-09-03 08:49:45 +0200

    Opgave 3

--- src/opgave03/BMICalculator.java
+++ src/opgave03/BMICalculator.java
@@ -1,6 +1,21 @@
 package opgave03;
 
+import java.util.Scanner;
+
 public class BMICalculator {
+
+    static double bmiIndex(double vægt, double hojde){
+        return vægt/(hojde*hojde);
+    }
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("skriv din vægt: ");
+        double vægt = scanner.nextDouble();
+        System.out.println("skriv din højde i meter: ");
+        double hojde = scanner.nextDouble();
+
+        System.out.println(bmiIndex(vægt, hojde));
+
+        scanner.close();
     }
 }
